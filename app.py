a02cae17cb86d627a5f1e4e71a7b5932e4b41ca4
Tanish <tanish@Tanishs-MacBook-Air.local>
2023-09-24 21:02:47 +0530

    Fourth Commit

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ import mysql.connector
 from flask import make_response, send_file
 import PyPDF2
 from io import BytesIO
-
+import os
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "sdfghjksdfghj"
 
-
-conn = mysql.connector.connect(host="localhost", user="user", password="password", database="mydatabase1")
+# Use the DATABASE_URL environment variable provided by Heroku
+database_url = os.environ.get("DATABASE_URL")
+conn = mysql.connector.connect(database_url)
 cursor = conn.cursor()
-
 
 class PDFUploadForm(FlaskForm):
     pdf_file = FileField("PDF File")
@@ -43,15 +43,11 @@ def index():
             return "Success: File was not uploaded to the database."
     return render_template("index.html", form=form)
 
-
-
-
 @app.route("/success")
 def success():
     cursor.execute("SELECT id, name, summary FROM pdf_files")
     file_names = cursor.fetchall()
     return render_template("success.html", file_names=file_names)
-
 
 @app.route("/download/<int:file_id>")
 def download(file_id):
@@ -70,10 +66,8 @@ def preview(file_id):
     cursor.execute("SELECT data FROM pdf_files WHERE id = %s", (file_id,))
     file_data = cursor.fetchone()
 
-
     if file_data is None:
         return "Error: File not found."
-
 
     response = make_response(file_data[0])
     response.headers['Content-Type'] = 'application/pdf'
@@ -82,7 +76,6 @@ def preview(file_id):
 
 @app.route("/delete/<int:file_id>", methods=["POST"])
 def delete(file_id):
-  
     cursor.execute("DELETE FROM pdf_files WHERE id = %s", (file_id,))
     conn.commit()
     return redirect("/success")
@@ -93,6 +86,5 @@ def view_summary(file_id):
     summary = cursor.fetchone()[0]
     return render_template("view_summary.html", summary=summary)
 
-
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
